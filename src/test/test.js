b0d67ffb8b42dc1a192b33b17838291f0b2f73f6
Price <205605@carolyn-price-1046.nyhq.nytint.com>
2016-07-18 15:16:04 -0400

    Running mocha tests

--- src/test/test.js
+++ src/test/test.js
@@ -2,13 +2,13 @@ var React= require('react');
 var chai = require('chai');
 var enzyme= require('enzyme');
 
-var Demo = require('../demo.js');
+import Demo from '../demo.js';
 
 describe('DEMO', () => {
   it('this is a test', () => {
     const wrapper = enzyme.shallow(
       <Demo />);
-    wrapper.debug();
-    chai.assert.true(true);
+    console.log(wrapper.debug());
+    chai.assert.isTrue(true);
   });
 });
