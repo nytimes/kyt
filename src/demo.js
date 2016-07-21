3998644d4d631d11c2171e1c4388f0ae8593bf18
Price <205605@carolyn-price-1046.nyhq.nytint.com>
2016-07-21 16:49:14 -0400

    Adding JSS

--- src/demo.js
+++ src/demo.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import styles from './demo-styles.css';
+import {create} from 'jss';
+import reactJss from 'react-jss';
+import vendorPrefixer from 'jss-vendor-prefixer';
+
+export let jss = create();
+export let useSheet = reactJss(jss);
+jss.use(vendorPrefixer());
+
+var styles = {
+  demo: {
+    'background-color': 'pink',
+    'border': '5px dashed black'
+  }
+};
+
+
 
 /*
  * A demo component using react and css modules
  *
 */
-export default () => <h1 className={styles.demo}>Hello It's Me</h1>;
+class Demo extends React.Component {
+  render() {
+    var classes = this.props.sheet.classes;
+    return ( <h1 className={classes.demo}>Hello It's Me</h1>);
+  }
+
+}
+
+export default useSheet(Demo, styles);
--- src/test/demo.test.js
+++ src/test/demo.test.js
@@ -2,9 +2,15 @@ import test from 'ava';
 var React= require('react');
 var chai = require('chai');
 var enzyme= require('enzyme');
+var jsdom = require('jsdom');
 import Demo from '../demo.js';
 test('one', t => {
+    var sheet = {
+      classes: {
+        demo: "demo"
+      }
+    };
     const wrapper = enzyme.shallow(
-      <Demo />);
+      <Demo/>);
     t.pass(chai.assert.isTrue(wrapper.hasClass('demo')));
 });
