6a5bdfbb82a17f4b2a02f0feeb860ce3e460384c
Price <205605@carolyn-price-1046.nyhq.nytint.com>
2016-08-02 11:59:58 -0400

    fixing post install

--- cli/actions/postinstall.js
+++ cli/actions/postinstall.js
@@ -1,6 +1,5 @@
 
 const fs = require('fs');
-const chalk = require('chalk');
 const logger = console;
 const path = require('path');
 const shell = require('shelljs');
@@ -11,7 +10,7 @@ module.exports = (program) => {
   //to see the verbose command ouput.
   shell.config.silent = true;
 
-  logger.log('kyt postinstall');
+  logger.log('🔥 starting kyt postinstall');
 
   // Create a symbolic link from our local .babelrc
   // to the project's main directory.
@@ -20,7 +19,7 @@ module.exports = (program) => {
 
 
   if (shell.ln('-s', babelrcPath, linkedPath).code === 0) {
-    logger.log(chalk.green('Linked .babelrc'));
+    logger.log('Linked .babelrc');
   }
 
   // Create a symbolic link from our local .editorconfig
@@ -29,65 +28,65 @@ module.exports = (program) => {
   const configPath = path.resolve(process.cwd(), '../../.editorconfig');
 
   if (shell.ln('-s', editorPath, configPath).code === 0) {
-    logger.log(chalk.green('Linked .editorconfig'));
+    logger.log(' Linked .editorconfig');
   }
 
   // Edit User's Package.json
   var packageJsonPath = path.resolve(process.cwd(), '../../package.json');
-  console.log('PATH', packageJsonPath);
   var packageJson = require(packageJsonPath);
   // Adding Ava Configuration
   // TODO: add ava-old if there are changes
   packageJson.ava = avaConfig;
-  logger.log(chalk.green('Added ava\'s config into your package.json'));
+  logger.log('Added ava\'s config into your package.json');
   // Adding kyt scripts
   if(packageJson.scripts == undefined) {
     packageJson.scripts = {};
   }
   const commands = ['dev', 'build', 'run', 'test', 'lint', 'update', 'proto'];
   commands.forEach((command) => {
-    if (packageJson.scripts[commandName]) commandName = 'kyt:' + command;
+    var commandName = command;
+    if(packageJson.scripts[command]) {
+      commandName = 'kyt:' + command;
+    }
     packageJson.scripts[commandName] = 'kyt ' + command;
   });
   packageJson.scripts['kyt:help'] = ' kyt --help';
-  logger.log(chalk.green('Added kyt\'s scripts into your npm scripts'));
+  logger.log('Added kyt\'s scripts into your npm scripts');
 
   // Write changes to user's package JSON
   fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
 
 
   // Create a src directory with app files.
-  console.log('CURRENT DIR', process.cwd());
   const src = path.resolve(process.cwd(), '../../src');
-  console.log('SRC', src);
   if (shell.ls(src).code !== 0) {
     shell.exec(`cp -rf ./src ${src}`);
-    logger.log(chalk.green('Created src directory with application files.'));
+    logger.log('Created src directory with application files.');
   } else {
     // Update Individual Files if src already exists
     const client = path.resolve(process.cwd(), '../../src/client.js');
     if(!shell.test('-f', client)) {
       const clientOG = path.resolve(process.cwd(), './src/client.js');
       shell.exec(`cp  ${clientOG} ${client} `);
-      logger.log(chalk.blue('Created client.js file'));
+      logger.log('Created client.js file');
     }
     const server = path.resolve(process.cwd(), '../../src/server.js');
     if(!shell.test('-f', server)) {
       const serverOG = path.resolve(process.cwd(), './src/server.js');
       shell.exec(`cp ${serverOG} ${server}`);
-      logger.log(chalk.blue('Created server.js file'));
+      logger.log('Created server.js file');
     }
     const proto = path.resolve(process.cwd(), '../../src/proto.js');
     if(!shell.test('-f', proto)) {
       const protoOG = path.resolve(process.cwd(), './src/proto.js');
       shell.exec(`cp ${protoOG} ${proto}`);
-      logger.log(chalk.blue('Created proto.js file'));
+      logger.log('Created proto.js file');
     }
     const indexHTML = path.resolve(process.cwd(), '../../src/index.html');
     if(!shell.test('-f', indexHTML)) {
       const indexOG = path.resolve(process.cwd(), './src/index.html');
       shell.exec(`cp ${indexOG} ${indexHTML}`);
-      logger.log(chalk.blue('Created index.html file'));
+      logger.log('Created index.html file');
     }
     // Only update TestComponent if they already have it
     // because users may delete it
@@ -97,10 +96,12 @@ module.exports = (program) => {
       // Remove and recopy the test component
       shell.exec(`rm -rf ${testComponent}`);
       shell.exec(`cp  -rf ${testComponentOG} ${testComponent}`);
-      logger.log(chalk.blue('Created TestComponent directory'));
+      logger.log('Created TestComponent directory');
     }
 
   }
+
+  logger.log('✅ completed kyt postinstall');
 
 
 };
