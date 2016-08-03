5da18c8f999e3a5f8e2f3e2d535ad24432a5373d
delambo <delambo@gmail.com>
2016-08-03 13:21:07 -0400

    poc of hapi server

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,6 @@
 
 import 'source-map-support/register';
-import express from 'express';
 import fs from 'fs';
-import compression from 'compression';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import RouterContext from 'react-router/lib/RouterContext';
@@ -14,30 +12,36 @@ import cleancss from 'clean-css';
 import jss from 'jss';
 const clientAssets = require(process.env.ASSETS_PATH);
 
-const app = express();
-app.disable('x-powered-by');
-app.use(compression());
+const Hapi = require('hapi');
 
-app.use(process.env.PUBLIC_PATH, express.static(process.env.CLIENT_BUILD_PATH));
+const server = new Hapi.Server();
+server.connection({ port: process.env.SERVER_PORT });
 
-app.get('*', (request, response, next) => {
-  const history = createMemoryHistory(request.originalUrl);
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, response) {
+      const history = createMemoryHistory(request.originalUrl);
 
-  match({ routes, history }, (error, redirectLocation, renderProps) => {
-    if (error) {
-      response.status(500).send(error.message);
-    } else if (redirectLocation) {
-      response.redirect(302, `${redirectLocation.pathname}${redirectLocation.search}`);
-    } else if (renderProps) {
-      response.status(200).send(template({
-        root: renderToString(<RouterContext {...renderProps} />),
-        css: new cleancss().minify(jss.sheets.toString()).styles,
-        jsBundle: clientAssets.main.js
-      }));
-    } else {
-      response.status(404).send('Not found');
+      match({ routes, history }, (error, redirectLocation, renderProps) => {
+        if (error) {
+          // response.status(500).send(error.message);
+        } else if (redirectLocation) {
+          // response.redirect(302, `${redirectLocation.pathname}${redirectLocation.search}`);
+        } else if (renderProps) {
+          response(template({
+            root: renderToString(<RouterContext {...renderProps} />),
+            css: new cleancss().minify(jss.sheets.toString()).styles,
+            jsBundle: clientAssets.main.js
+          }));
+        } else {
+          // response.status(404).send('Not found');
+        }
+      });
     }
-  });
 });
 
-app.listen(parseInt(process.env.SERVER_PORT, 10));
+server.start((err) => {
+  if (err) throw err;
+  console.log('Server running at:', server.info.uri);
+});
