6927099c29ee0cae3277e114f306c1cbc1b56aa2
delambo <delambo@gmail.com>
2016-08-07 09:38:35 -0400

    remove src directory

--- src/utils/jss.js
+++
@@ -1,7 +0,0 @@
-
-import jss from 'jss';
-import camelCase from 'jss-camel-case'
-
-jss.use(camelCase());
-
-export default (styles, options) => jss.createStyleSheet(styles, options).attach().classes;
