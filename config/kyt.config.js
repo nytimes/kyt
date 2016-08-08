6b648ff7ac4054800c27866fe432339c57c0c09b
delambo <delambo@gmail.com>
2016-08-08 18:48:34 -0400

    fixes publicPath issue since base kyt config was not merged with user kyt config

--- config/kyt.config.js
+++ config/kyt.config.js
@@ -4,16 +4,17 @@
 const shell = require('shelljs');
 const path = require('path');
 const baseConfig = require('./kyt.base.config');
+const merge = require('ramda').merge;
 
-const userConfig = path.resolve(__dirname, '../../../kyt.config.js');
+const userConfig = path.join(process.cwd(), 'kyt.config.js');
 let config;
 
 // Find user config
 if (shell.test('-f', userConfig)) {
   config = require(userConfig); // eslint-disable-line global-require
-} else {
-  config = baseConfig;
 }
+
+config = merge(baseConfig, config);
 
 // Create default modify function
 if (typeof config.modifyWebpackConfig !== 'function') {
