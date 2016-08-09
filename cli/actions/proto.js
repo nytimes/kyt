b238e74467208e2cfd2aff9d0f3f0c27a68420e4
Price <carolyn.price@nytimes.com>
2016-08-09 16:32:39 -0400

    Prototype file warning

--- cli/actions/proto.js
+++ cli/actions/proto.js
@@ -1,15 +1,15 @@
 
 // Command to run prorotype dev server
-
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const WebpackDevServer = require('webpack-dev-server');
+const path = require('path');
+const shell = require('shelljs');
 const logger = require('./../logger');
 const kytConfig = require('./../../config/kyt.config');
 const baseConfig = require('./../../config/webpack.base');
 const protoConfig = require('./../../config/webpack.proto');
 module.exports = () => {
-  logger.start('Configuring Prototype...');
   const port = kytConfig.prototypePort;
   const userRootPath = kytConfig.userRootPath;
   const options = {
@@ -19,24 +19,35 @@ module.exports = () => {
     userRootPath,
   };
 
-  // Build webpack config
-  let webpackConfig = merge.smart(baseConfig(options), protoConfig(options));
-  webpackConfig = kytConfig.modifyWebpackConfig(webpackConfig, options);
+  var startPrototype = (err) => {
+    // Build webpack config
+    let webpackConfig = merge.smart(baseConfig(options), protoConfig(options));
+    webpackConfig = kytConfig.modifyWebpackConfig(webpackConfig, options);
 
-  // Preparing prototype compiler
-  let compiler = null;
-  try {
-    logger.debug('Prototype Config', webpackConfig);
-    compiler = webpack(webpackConfig);
-  } catch (error) {
-    logger.error('Webpack config is invalid\n', error);
+    // Preparing prototype compiler
+    let compiler = null;
+    try {
+      logger.debug('Prototype Config', webpackConfig);
+      compiler = webpack(webpackConfig);
+    } catch (error) {
+      logger.error('Webpack config is invalid\n', error);
+      process.exit();
+    }
+
+    //webpackConfig.devServer.stats = true;
+    // Creates a webpack dev server at the specified port
+    const server = new WebpackDevServer(compiler, webpackConfig.devServer);
+    server.listen(port, 'localhost', () => {
+      logger.end(`webpack-dev-server http://localhost:${port}/prototype`);
+    });
+  };
+  logger.start('Configuring Prototype...');
+
+  const userPrototypePath = path.join(userRootPath, './prototype.js');
+  if (!shell.test('-f', userPrototypePath)) {
+    logger.error('Prototype.js entry file does not exist.');
+    logger.error('See the kyt Readme for details.');
     process.exit();
   }
-
-
-  // Creates a webpack dev server at the specified port
-  const server = new WebpackDevServer(compiler, webpackConfig.devServer);
-  server.listen(port, 'localhost', () => {
-    logger.end(`webpack-dev-server http://localhost:${port}/prototype`);
-  });
+  startPrototype();
 };
---
+++ prototype/prototype.base.js
@@ -0,0 +1,10 @@
+
+// This is your prototyping file.
+// It's the entry for the kyt proto command
+// Use this file to work on individual modules
+// or build something new
+
+// Root Element of the index.js file loaded by the prototype.
+const rootEl = document.getElementById('root');
+
+rootEl.innerHTML = ('Hello World');
