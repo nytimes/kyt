33e76883f176e8de23fca3473bd5122da7727e0b
Jeremy Gayed <jeremy.gayed@nytimes.com>
2016-08-11 20:11:25 -0400

    Refactor build & dev to DRY it up a bit and make it easier to test

--- cli/actions/build.js
+++ cli/actions/build.js
@@ -1,53 +1,22 @@
 
 // Command to build production code
 
-const path = require('path');
 const shell = require('shelljs');
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const filesize = require('filesize');
-const stripAnsi = require('strip-ansi');
 const logger = require('./../logger');
-const kytConfig = require('./../../config/kyt.config');
-const baseConfig = require('./../../config/webpack.base');
-const clientWebpackConfig = require('./../../config/webpack.prod.client');
-const serverWebpackConfig = require('./../../config/webpack.prod.server');
+const printAssets = require('../../utils/printAssets');
+const buildConfigs = require('../../utils/buildConfigs');
+const webpackCompiler = require('../../utils/webpackCompiler');
 
 module.exports = () => {
-  const serverPort = kytConfig.serverPort;
-  const userRootPath = kytConfig.userRootPath;
-
-  const clientOptions = {
-    type: 'client',
-    serverPort,
-    clientPort: undefined,
-    environment: 'production',
-    publicPath: kytConfig.productionPublicPath,
-    assetsPath: path.join(userRootPath, 'build/client'),
-    userRootPath,
-  };
-
-  const serverOptions = merge(clientOptions, {
-    assetsPath: path.join(userRootPath, 'build/server'),
-    type: 'server',
-  });
-
-  let clientCompiler = null;
-  let serverCompiler = null;
-
-  let clientConfig = merge.smart(baseConfig(clientOptions), clientWebpackConfig(clientOptions));
-  let serverConfig = merge.smart(baseConfig(serverOptions), serverWebpackConfig(serverOptions));
-
-  // Modify configs
-  try {
-    clientConfig = kytConfig.modifyWebpackConfig(clientConfig, clientOptions);
-    serverConfig = kytConfig.modifyWebpackConfig(serverConfig, serverOptions);
-  } catch (error) {
-    logger.error('Error in your kyt.config.js modifyWebpackConfig():', error);
-    process.exit();
-  }
-
   logger.start('Starting production build...');
+
+  let serverCompiler;
+
+  const {
+    clientConfig,
+    serverConfig,
+    userRootPath,
+  } = buildConfigs('production');
 
   // Clean the build directory.
   if (shell.exec(`rm -rf ${userRootPath}/build`).code === 0) {
@@ -56,71 +25,16 @@ module.exports = () => {
 
   // Compiles server code using the prod.server config
   const buildServer = () => {
-    try {
-      logger.debug('Server webpack configuration:', serverConfig);
-      serverCompiler = webpack(serverConfig);
-      logger.task('Server webpack configuration compiled');
-    } catch (error) {
-      logger.error('Server webpack configuration is invalid\n', error);
-      process.exit();
-    }
-
-    serverCompiler.plugin('done', (stats) => {
-      if (stats.hasErrors()) {
-        logger.error('Server build failed\n', stats);
-      } else {
-        logger.task('Server build successful');
-        logger.end('Done building');
-      }
+    serverCompiler = webpackCompiler(serverConfig, () => {
+      logger.end('Done building');
     });
-
     serverCompiler.run(() => undefined);
   };
 
-  // Preparing client webpack compiler
-  try {
-    logger.debug('Client webpack configuration:', clientConfig);
-    clientCompiler = webpack(clientConfig);
-    logger.task('Client webpack configuration compiled');
-  } catch (error) {
-    logger.error('Client webpack configuration is invalid\n', error);
-    process.exit();
-  }
-
-  const printAssets = (stats) => {
-    const assets = stats.toJson().assets
-      .filter(asset => /\.(js|css)$/.test(asset.name))
-      .map(asset => ({
-        folder: path.join('build/client', path.dirname(asset.name)),
-        name: path.basename(asset.name),
-        size: asset.size,
-        sizeLabel: filesize(asset.size),
-      }));
-    assets.sort((a, b) => b.size - a.size);
-    const longestSizeLabelLength = Math.max.apply(null,
-      assets.map(a => stripAnsi(a.sizeLabel).length)
-    );
-    assets.forEach(asset => {
-      let sizeLabel = asset.sizeLabel;
-      const sizeLength = stripAnsi(sizeLabel).length;
-      if (sizeLength < longestSizeLabelLength) {
-        const rightPadding = ' '.repeat(longestSizeLabelLength - sizeLength);
-        sizeLabel += rightPadding;
-      }
-      logger.log(`    ${sizeLabel}    ${asset.folder + path.sep + asset.name}`);
-    });
-  };
-
-  clientCompiler.plugin('done', (stats) => {
-    if (stats.hasErrors()) {
-      logger.error('Client build failed\n', stats.toString());
-    } else {
-      logger.task('Client build successful');
-      logger.info('Assets:');
-      printAssets(stats);
-      buildServer();
-    }
+  const clientCompiler = webpackCompiler(clientConfig, stats => {
+    logger.info('Assets:');
+    printAssets(stats);
+    buildServer();
   });
-
   clientCompiler.run(() => undefined);
 };
---
+++ utils/webpackCompiler.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+const logger = require('../cli/logger');
+
+module.exports = (webpackConfig, cb) => {
+  let webpackCompiler;
+  const type = webpackConfig.target === 'web' ? 'Client' : 'Server';
+
+  // Compile the webpack config
+  try {
+    logger.debug(`${type} webpack configuration:`, webpackConfig);
+    webpackCompiler = webpack(webpackConfig);
+    logger.task(`${type} webpack configuration compiled`);
+  } catch (error) {
+    logger.error(`${type} webpack config is invalid\n`, error);
+    process.exit();
+  }
+
+  // Handle errors in webpack build
+  webpackCompiler.plugin('done', stats => {
+    if (stats.hasErrors()) {
+      logger.error(`${type} build failed\n`, stats.toString());
+      process.exit(1);
+    }
+
+    if (stats.hasWarnings()) {
+      logger.warn(`${type} build warnings`, stats.toString());
+    }
+
+    logger.task(`${type} build successful`);
+
+    // Call the callback on successful build
+    if (cb) {
+      cb(stats);
+    }
+  });
+
+
+  // Return the compiler
+  return webpackCompiler;
+};
