3e746e275706bd85930dbfd7de05114dc69ff182
Price <carolyn.price@nytimes.com>
2016-08-21 21:48:39 -0400

    adding webpack via babel register rather than precompile

--- cli/actions/test.js
+++ cli/actions/test.js
@@ -6,21 +6,18 @@ const logger = require('./../logger');
 const shell = require('shelljs');
 const kytConfig = require('./../../config/kyt.config');
 
+
 module.exports = () => {
   // Comment the following to see verbose shell ouput.
-  shell.config.silent = true;
+  //shell.config.silent = true;
 
   const userRootPath = kytConfig.userRootPath;
   const userSrc = path.join(userRootPath, 'src');
-  const userBuild = path.join(userRootPath, 'build/test');
   const avaCLI = path.resolve(userRootPath, './node_modules/ava/cli.js');
   const npath = path.resolve(userRootPath, './node_modules');
-  const babel = path.join(npath, '.bin/babel');
-  const es2015Preset = require.resolve('babel-preset-es2015');
-  const reactPreset = require.resolve('babel-preset-react');
-  const presets = `${es2015Preset},${reactPreset}`;
-  const babelWebpack = require.resolve('babel-plugin-webpack-loaders');
-  const plugins = babelWebpack;
+  const babelRegister = require.resolve('babel-register');
+  const babelRC = path.resolve(userRootPath, './node_modules/kyt/.babelrc');
+  const tmpBabelRC = path.resolve(userRootPath, './tmp-test/.babelrc');
   const testConfigPath = path.resolve(__dirname, '../../config/webpack.temp.test.js');
   const tempTestDir = path.join(userRootPath, './tmp-test');
   const newConfigPath = path.join(tempTestDir, './webpack.config.js');
@@ -28,44 +25,31 @@ module.exports = () => {
   logger.start('Running Test Command...');
 
   // Clean the build directory.
-  if (shell.test('-d', userBuild)) {
-    shell.rm('-rf', userBuild);
+  if (shell.test('-d', tempTestDir)) {
+    shell.rm('-rf', tempTestDir);
     logger.task('Cleaned test folder');
   }
 
   // Create Temp Directory and move user src files there
   shell.mkdir(tempTestDir);
   shell.cp('-r', userSrc, tempTestDir);
-
+  // Copy babelRC into test directory
+  shell.cp(babelRC, tmpBabelRC);
   // Copy the webpack config into the temp directory
   shell.cp(testConfigPath, newConfigPath);
 
   // Compile Code and move it into the user's root directory
   shell.cd(tempTestDir);
-  const babelCommand = `NODE_PATH=$NODE_PATH:${npath} BABEL_DISABLE_CACHE=1 ` +
-  `${babel} ${tempTestDir} --ignore node_modules --presets ${presets} ` +
-  `--plugins ${plugins} --out-dir ${userBuild} -s inline`;
-
-  const babelExec = shell.exec(babelCommand);
-  if (babelExec.code !== 0) {
-    logger.error('Error transpiling test code', babelExec.stderr);
-    shell.rm('-rf', tempTestDir);
-    process.exit();
-  }
-
-  // Remove tmp directory
-  shell.rm('-rf', tempTestDir);
-  logger.task('Test files prepared by Babel');
-  // Move back to user's root directory
-  shell.cd(userRootPath);
 
   // Execute the ava cli on our build.
   // We add our node_modules tothe NODE_PATH so that ava can be resolved.
-  let command = `NODE_PATH=$NODE_PATH:${npath} ` +
-  `node ${avaCLI} ${userRootPath}/build/test/**/*.test.js`;
+  let command = `NODE_PATH=$NODE_PATH:${npath} NODE_ENV=TEST` +
+  `node ${avaCLI} --require ${babelRegister} ${userRootPath}/tmp-test/**/*.test.js`;
 
   if (kytConfig.debug) command += ' --verbose';
 
   shell.config.silent = false;
   shell.exec(command);
+  shell.cd(userRootPath);
+  shell.rm('-rf', tempTestDir);
 };
