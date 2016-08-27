30e8c4c64f2ecd5c362f6353eb017d0114353c39
delambo <delambo@gmail.com>
2016-08-27 15:22:15 -0400

    removes webpack dev server and makes the react hot loader stuff optional through the reactHotLoader setting

--- cli/actions/dev.js
+++ cli/actions/dev.js
@@ -13,7 +13,6 @@ const kytConfig = require('./../../config/kyt.config');
 const ifPortIsFreeDo = require('../../utils/ifPortIsFreeDo');
 const buildConfigs = require('../../utils/buildConfigs');
 const webpackCompiler = require('../../utils/webpackCompiler');
-const WebpackDevServer = require('webpack-dev-server');
 
 module.exports = () => {
   logger.start('Starting development build...');
@@ -39,27 +38,13 @@ module.exports = () => {
   let serverCompiler;
   let server = null;
 
-  const getHotClient = (options) => {
+  const startClient = () => {
+    const devOptions = clientCompiler.options.devServer;
     const app = express();
-    const webpackDevMiddleware = devMiddleware(clientCompiler, options);
+    const webpackDevMiddleware = devMiddleware(clientCompiler, devOptions);
 
     app.use(webpackDevMiddleware);
     app.use(hotMiddleware(clientCompiler));
-
-    return app;
-  };
-
-  const getDevServer = (options) => new WebpackDevServer(clientCompiler, options);
-
-  const startClient = () => {
-    const devOptions = {
-      publicPath: clientCompiler.options.output.publicPath,
-      headers: { 'Access-Control-Allow-Origin': '*' },
-      noInfo: true,
-      quiet: true,
-    };
-
-    const app = reactHotLoader ? getHotClient(devOptions) : getDevServer(devOptions);
     app.listen(clientPort);
   };
 
@@ -121,6 +106,9 @@ module.exports = () => {
     } else startHotServer();
   });
 
-  // Start client hot server
+  // Starting point...
+  // By starting the client, the middleware will
+  // compile the client configuration and trigger
+  // the `clientCompiler` callback.
   ifPortIsFreeDo(clientPort, startClient);
 };
