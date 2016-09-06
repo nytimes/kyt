09b7666d965fb77fdb9580894e6846c15c6a248c
coolov <olov.sundstroem@nytimes.com>
2016-09-06 12:27:34 -0400

    remove arrow function

--- utils/exitIfOldNodeVersion.js
+++ utils/exitIfOldNodeVersion.js
@@ -1,10 +1,11 @@
-
+// This module has to be supported by old versions of node,
+// therefor no ES6 (arrow functions, const's etc)
 var minNodeVersion = 6;
 
 // Returns whether node is version `minNodeVersion` or above
-module.exports = () => {
+module.exports = function() {
   if (Number(process.versions.node.split('.')[0]) < minNodeVersion) {
-    console.error(`kyt requires Node v${minNodeVersion}+`);
+    console.error('kyt requires Node v' + minNodeVersion + '+');
     console.info('Need to run multiple versions of node? Check out nvm');
     process.exit();
   }
