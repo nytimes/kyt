5041ea4c19cf2c35410943e32d7743c35e083a42
coolov <olov.sundstroem@nytimes.com>
2016-09-06 12:38:01 -0400

    tidy up

--- cli/index.js
+++ cli/index.js
@@ -1,5 +1,4 @@
-// This module has to be supported by old versions of node,
-// it can't have any ES6 (arrow functions, const's etc)
+// This module has to be supported by old versions of node, so no ES6
 /* eslint-disable no-var, prefer-arrow-callback, prefer-template, no-console */
 var minNodeVersion = 6;
 
@@ -9,8 +8,7 @@ process.on('uncaughtException', function (error) {
   process.exit();
 });
 
-// Make sure that the user has a
-// ES6 compatible node version before loading the app.
+// Make sure that the user has a compatible node version before loading the app.
 if (Number(process.versions.node.split('.')[0]) < minNodeVersion) {
   console.error('kyt requires Node v' + minNodeVersion + '+');
   console.info('Need to run multiple versions of node? Check out nvm');
