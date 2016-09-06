5f276735a188894879ce2d40da55605cb38d2510
coolov <olov.sundstroem@nytimes.com>
2016-09-06 12:37:00 -0400

    fix lint err's

--- cli/index.js
+++ cli/index.js
@@ -1,9 +1,10 @@
 // This module has to be supported by old versions of node,
 // it can't have any ES6 (arrow functions, const's etc)
+/* eslint-disable no-var, prefer-arrow-callback, prefer-template, no-console */
 var minNodeVersion = 6;
 
 // Surface any uncaught errors
-process.on('uncaughtException', function(error) {
+process.on('uncaughtException', function (error) {
   console.error('UNHANDLED EXCEPTION', error.stack);
   process.exit();
 });
