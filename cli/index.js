2f166b8155ffdbbbc1217278b7b91826af86c988
delambo <delambo@gmail.com>
2016-09-06 21:15:33 -0400

    removed uncaught exception handler; added exit code 1 to node version check

--- cli/index.js
+++ cli/index.js
@@ -2,17 +2,11 @@
 /* eslint-disable no-var, prefer-arrow-callback, prefer-template, no-console */
 var minNodeVersion = 6;
 
-// Surface any uncaught errors
-process.on('uncaughtException', function (error) {
-  console.error('UNHANDLED EXCEPTION', error.stack);
-  process.exit();
-});
-
 // Make sure that the user has a compatible node version before loading the app.
 if (Number(process.versions.node.split('.')[0]) < minNodeVersion) {
   console.error('kyt requires Node v' + minNodeVersion + '+');
   console.info('Need to run multiple versions of node? Check out nvm');
-  process.exit();
+  process.exit(1);
 }
 
 require('./commands');
