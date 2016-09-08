5ccf109d25f30ea3ffa75ff6cf6d0245977fa8e2
ccpricenytimes <ccpricenytimes@users.noreply.github.com>
2016-09-08 15:27:49 -0400

    Fixes kyt base (#101)
    * reduces kyt base config
    
    * moving base config options into fn
    
    * changing merge to mergeAll

--- utils/kytConfig.js
+++ utils/kytConfig.js
@@ -3,21 +3,26 @@
 
 const path = require('path');
 const shell = require('shelljs');
-const baseConfig = require('./../config/kyt.base.config');
-const merge = require('ramda').merge;
+const mergeAll = require('ramda').mergeAll;
 const { userRootPath, userKytConfigPath } = require('./paths')();
-
 module.exports = (optionalConfig) => {
   if (global.config) return;
+
+  // base config options
+  const baseConfig = {
+    productionPublicPath: '/assets/',
+    clientPort: 3001,
+    serverPort: 3000,
+    prototypePort: 3002,
+    debug: false,
+    reactHotLoader: false,
+  };
 
   const userConfigPath = optionalConfig
     ? path.join(userRootPath, optionalConfig)
     : userKytConfigPath;
   let config;
   const logger = console;
-
-  // Add base config option for productionPublicPath
-  baseConfig.productionPublicPath = '/assets/';
 
   // Find user config
   if (shell.test('-f', userConfigPath)) {
@@ -30,14 +35,12 @@ module.exports = (optionalConfig) => {
     }
   }
 
-  config = merge(baseConfig, config);
+  config = mergeAll([{}, baseConfig, config]);
 
   // Create default modify function
   if (typeof config.modifyWebpackConfig !== 'function') {
     config.modifyWebpackConfig = (webpackConfig) => webpackConfig;
   }
 
-  // In case `reactHotLoader` is undefined, make it a boolean
-  config.reactHotLoader = !!config.reactHotLoader;
   global.config = Object.freeze(config);
 };
