72367cec188706a539acff3b71d5af7236519c5e
Felipe Silva <felipef.silva@gmail.com>
2016-09-08 13:58:10 -0400

    compress warnings defaults to false

--- config/webpack.prod.client.js
+++ config/webpack.prod.client.js
@@ -62,7 +62,6 @@ module.exports = (options) => ({
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         screw_ie8: true,
-        warnings: false,
       },
       output: {
         comments: false,
