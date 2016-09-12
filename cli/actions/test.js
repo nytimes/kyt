be01cc937f5f55eba1af9ea1e87fec826c7e5abb
Jeremy Gayed <jeremy.gayed@nytimes.com>
2016-09-12 15:05:09 -0400

    Also grab `resolve.alias` from userland webpack config

--- cli/actions/test.js
+++ cli/actions/test.js
@@ -6,6 +6,7 @@ const jest = require('jest');
 const shell = require('shelljs');
 const jestConfigBuilder = require('../../config/jest');
 const { srcPath } = require('../../utils/paths')();
+const buildConfigs = require('../../utils/buildConfigs');
 
 
 module.exports = (config, program) => {
@@ -18,8 +19,11 @@ module.exports = (config, program) => {
   // set BABEL_ENV to test if undefined
   process.env.BABEL_ENV = process.env.BABEL_ENV || 'test';
 
+  // Grab aliases from webpack config
+  const aliases = buildConfigs(config).clientConfig.resolve.alias;
+
   // Build Jest config
-  let jestConfig = jestConfigBuilder(srcPath);
+  let jestConfig = jestConfigBuilder(srcPath, aliases);
   jestConfig = config.modifyJestConfig(clone(jestConfig), { environment: 'test' });
 
   // Run Jest
--- config/jest.js
+++ config/jest.js
@@ -2,13 +2,16 @@ const path = require('path');
 
 const resolveFromUtils = file => path.resolve(__dirname, '..', 'utils', 'jest', file);
 
-module.exports = rootDir => ({
-  moduleNameMapper: {
-    '^[./a-zA-Z0-9$_-]+\\.(jpg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm)$':
-      resolveFromUtils('file.stub'),
-    '^[./a-zA-Z0-9$_-]+\\.(css|scss)$':
-      resolveFromUtils('style.stub'),
-  },
+module.exports = (rootDir, aliases = {}) => ({
+  moduleNameMapper: Object.assign(
+    {
+      '^[./a-zA-Z0-9$_-]+\\.(jpg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm)$':
+          resolveFromUtils('file.stub'),
+      '^[./a-zA-Z0-9$_-]+\\.(css|scss)$':
+          resolveFromUtils('style.stub'),
+    },
+    aliases
+  ),
   scriptPreprocessor: resolveFromUtils('preprocessor'),
   testPathIgnorePatterns: ['<rootDir>/(build|docs|node_modules|images)/'],
   testEnvironment: 'node',
