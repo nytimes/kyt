78b5e962c6088ac0222951fcc99772fbc3321b2f
Jeremy Gayed <jeremy.gayed@nytimes.com>
2016-09-13 15:43:37 -0400

    Simple paths test

---
+++ utils/__tests__/paths.test.js
@@ -0,0 +1,24 @@
+describe('paths', () => {
+  const paths = require('../paths')();
+  it('exports the expected properties', () => {
+    ['userRootPath',
+     'srcPath',
+     'buildPath',
+     'publicBuildPath',
+     'publicSrcPath',
+     'serverSrcPath',
+     'clientSrcPath',
+     'clientBuildPath',
+     'serverBuildPath',
+     'testBuildPath',
+     'prototypeBuildPath',
+     'assetsBuildPath',
+     'userPrototypePath',
+     'userKytConfigPath',
+     'userNodeModulesPath',
+     'userPackageJSONPath',
+   ].forEach(p => {
+     expect(paths[p]).toBeDefined();
+   });
+  });
+});
