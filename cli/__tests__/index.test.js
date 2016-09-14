62fc4c6dbb557ef873735e5470879d7df2f9da88
Jeremy Gayed <jeremy.gayed@nytimes.com>
2016-09-14 11:15:00 -0400

    index test

---
+++ cli/__tests__/index.test.js
@@ -0,0 +1,12 @@
+jest.mock('../commands', () => {});
+
+it('bails when min node version is not met', () => {
+  global.process.exit = jest.fn();
+  global.console.error = jest.fn();
+  global.console.info = jest.fn();
+  global.process.versions = { node: '4.0' };
+  require('../index');
+
+  expect(console.error).toBeCalled();
+  expect(process.exit).toBeCalled();
+});
