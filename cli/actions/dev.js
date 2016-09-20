09553909e338fbdfca57f85dac2dd3a31424bad9
Price <carolyn.price@nytimes.com>
2016-09-19 20:47:33 -0400

    fixes bug where dev server doesnt close after webpack compile error

--- cli/actions/dev.js
+++ cli/actions/dev.js
@@ -18,6 +18,11 @@ const { buildPath, serverSrcPath } = require('../../utils/paths')();
 module.exports = (config) => {
   logger.start('Starting development build...');
 
+  // Kill the server on exit.
+  process.on('SIGINT', () => {
+    process.exit();
+  });
+
   let clientCompiler;
   let serverCompiler;
   const { clientConfig, serverConfig } = buildConfigs(config);
