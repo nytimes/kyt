583f71fda207d3a89b5de866baa930f5c8428d27
Jared McDonald <jared.mcdonald@nytimes.com>
2016-09-30 17:44:49 -0400

    refactor actions/build test and add small test for no-server option

--- cli/actions/__tests__/build.test.js
+++ cli/actions/__tests__/build.test.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 const shell = {
   exec: jest.fn(() => ({ code: 0 })),
   mkdir: jest.fn(),
@@ -19,56 +21,83 @@ const build = require('../build');
 const buildConfigs = require('../../../utils/buildConfigs');
 const { buildPath, publicBuildPath, publicSrcPath } = require('../../../utils/paths')();
 
+const submodules = mod => Object.keys(mod).map(key => mod[key]);
+
 describe('build', () => {
-  const testConfig = { test: 'test' };
-
-  build(testConfig);
-
-  it('logs start', () => {
-    expect(logger.start).toBeCalledWith('Starting production build...');
+  // can replace this with jest.clearAllMocks when it lands in 16.0.0
+  beforeEach(() => {
+    [
+      ...submodules(shell),
+      printAssets,
+      webpackCompiler,
+      webpackCompiler.run,
+      ...submodules(logger),
+      buildConfigs,
+    ].forEach(mock => mock.mockClear());
   });
 
-  it('builds production configuration', () => {
-    expect(buildConfigs).toBeCalledWith(testConfig, 'production');
-  });
+  it('builds correctly with a server', () => {
+    const testConfig = { test: 'test' };
 
-  it('cleans the build directory', () => {
-    expect(shell.exec.mock.calls[0][0]).toMatch(/^rm -rf/);
-    expect(shell.mkdir).toBeCalledWith(buildPath);
-    expect(logger.task).toBeCalledWith('Cleaned ./build');
-  });
+    build(testConfig);
 
-  it('copies public folder into build', () => {
-    expect(shell.test).toBeCalledWith('-d', publicSrcPath);
+    assert.deepEqual(logger.start.mock.calls, [['Starting production build...']],
+      'should log start');
+    assert.deepEqual(buildConfigs.mock.calls, [[testConfig, 'production']],
+      'builds production configuration');
 
-    expect(shell.test).toBeCalledWith('-d', buildPath);
-    expect(shell.mkdir.mock.calls[1][0]).toBe(buildPath);
+    assert.ok(/^rm -rf/.test(shell.exec.mock.calls[0][0]),
+      'should clean build directory...');
+    assert.deepEqual(shell.mkdir.mock.calls[0], [buildPath],
+      'should recreate build directory');
+    assert.deepEqual(logger.task.mock.calls[0], ['Cleaned ./build'],
+      'should log that build was cleaned');
 
-    expect(shell.cp).toBeCalledWith('-r', publicSrcPath, publicBuildPath);
-    expect(logger.task).toBeCalledWith('Copied /src/public to /build/public');
-
-    // TODO: test creation of public folder
-  });
-
-  it('compiles webpack configs', () => {
-    const stats = webpackCompiler.mock.calls[0][1];
-    expect(typeof stats).toBe('function');
+    assert.deepEqual(shell.test.mock.calls[0], ['-d', publicSrcPath],
+      'should check public src path');
+    assert.deepEqual(shell.test.mock.calls[1], ['-d', buildPath],
+      'should check build path');
+    assert.equal(shell.mkdir.mock.calls[1], buildPath,
+      'should mkdir buildPath');
+    assert.deepEqual(shell.cp.mock.calls[0], ['-r', publicSrcPath, publicBuildPath],
+      'should copy public src to public build');
+    assert.deepEqual(logger.task.mock.calls[1], ['Copied /src/public to /build/public'],
+      'should log that it copied public src to public build');
 
     // for client
-    expect(webpackCompiler.mock.calls[0][0]).toBe('clientConfig');
-    expect(webpackCompiler.run).toBeCalled();
+    assert.equal(webpackCompiler.mock.calls[0][0], 'clientConfig',
+      'should call webpackCompiler with clientConfig');
+    assert.ok(webpackCompiler.run.mock.calls.length > 0,
+      'should call webpackCompiler.run');
 
     // client stats
+    const stats = webpackCompiler.mock.calls[0][1];
+    assert.equal(typeof stats, 'function',
+      'stats should be a function');
     stats('stats');
-    expect(logger.info).toBeCalledWith('Assets:');
-    expect(printAssets).toBeCalledWith('stats');
+    assert.deepEqual(logger.info.mock.calls, [['Assets:']],
+      'should call logger.info');
+    assert.deepEqual(printAssets.mock.calls, [['stats']],
+      'should call printAssets');
 
     // for server
     const doneBuilding = webpackCompiler.mock.calls[1][1];
-    expect(webpackCompiler.mock.calls[1][0]).toBe('serverConfig');
+    assert.equal(webpackCompiler.mock.calls[1][0], 'serverConfig',
+      'should call webpackCompiler with serverConfig second');
 
     // done building server
     doneBuilding();
-    expect(logger.end).toBeCalledWith('Done building');
+    assert.deepEqual(logger.end.mock.calls, [['Done building']],
+      'should log success');
+  });
+
+  it('builds correctly without a server', () => {
+    build({ test: 'test', noServer: true });
+    assert.equal(webpackCompiler.mock.calls[0][0], 'clientConfig',
+      'should call webpackCompiler with clientConfig');
+    assert.ok(webpackCompiler.run.mock.calls.length > 0,
+      'should call webpackCompiler.run');
+    assert.equal(webpackCompiler.mock.calls.length, 1,
+      'should not call webpackCompiler a second time for server');
   });
 });
