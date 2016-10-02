51b39493bfc805ee36f708fcbc6be70d31e20d86
delambo <delambo@gmail.com>
2016-10-02 11:46:41 -0400

    fixes start test

--- cli/actions/__tests__/start.test.js
+++ cli/actions/__tests__/start.test.js
@@ -9,9 +9,10 @@ describe('start', () => {
   const serverURL = { href: 'href' };
   start({ serverURL });
 
-  it('logs start and end', () => {
+  it('logs start, task and end', () => {
     expect(logger.start).toBeCalledWith('Starting production...');
-    expect(logger.end).toBeCalledWith(`Server running at ${serverURL.href}`);
+    expect(logger.task).toBeCalledWith(`Server running on ${serverURL.href}`);
+    expect(logger.end).toBeCalledWith('Production started');
   });
 
   it('executes the node process asynchronously', () => {
