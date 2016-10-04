0281680ef29b33265dfe4bc62865c5c7fa920b97
Price <carolyn.price@nytimes.com>
2016-10-03 22:22:53 -0400

    setup tests. deleted shell mock, so regular tests arent working

---
+++ e2e_tests/tests/01setup.test.js
@@ -0,0 +1,64 @@
+
+describe('Installation and Setup', () => {
+  it('installs', () => {
+    const path = require.requireActual('path');
+    const pkgJsonPath = path.join(__dirname, './../pkg.json');
+    const shell = require.requireActual('shelljs');
+
+    if (shell.test('-d', 'cli-test')) {
+      shell.rm('-rf', 'cli-test');
+    }
+    shell.mkdir('cli-test');
+    shell.cd('cli-test');
+    shell.cp(pkgJsonPath, 'package.json');
+    const output = shell.exec('npm install');
+    if (output.code !== 0) {
+      process.exit();
+    }
+
+    expect(shell.test('-f', 'package.json')).toBe(true);
+    expect(shell.test('-d', 'node_modules')).toBe(true);
+  });
+  it('sets up the default starter-kyt', () => {
+    const shell = require.requireActual('shelljs');
+    const output = shell.exec('node_modules/.bin/kyt setup -r git@github.com:nytm/wf-kyt-starter-test.git');
+    expect(output.code).toBe(0);
+    const setupArr = output.stdout.split('\n');
+    expect(setupArr.includes('🔥  Setting up starter-kyt')).toBe(true);
+    expect(setupArr.includes('👍  Added kyt scripts into your package.json scripts')).toBe(true);
+    expect(setupArr.includes('👍  Added new dependencies to package.json')).toBe(true);
+    expect(setupArr.includes('👍  Installed new modules')).toBe(true);
+    expect(setupArr.includes('👍  Created .eslintrc.json file')).toBe(true);
+    expect(setupArr.includes('👍  Created .stylelintrc.json file')).toBe(true);
+    expect(setupArr.includes('👍  Created kyt.config.js file')).toBe(true);
+    expect(setupArr.includes('👍  Created .editorconfig file')).toBe(true);
+    expect(setupArr.includes('👍  Created .gitignore file')).toBe(true);
+    expect(setupArr.includes('👍  Created src directory')).toBe(true);
+  });
+  it('sets up with the correct file', () => {
+    const shell = require.requireActual('shelljs');
+    expect(shell.test('-d', 'src')).toBe(true);
+    expect(shell.test('-f', 'kyt.config.js')).toBe(true);
+    expect(shell.test('-f', '.editorconfig')).toBe(true);
+    expect(shell.test('-f', '.eslintrc.json')).toBe(true);
+    expect(shell.test('-f', '.stylelintrc.json')).toBe(true);
+    expect(shell.test('-f', 'prototype.js')).toBe(true);
+  });
+  it('sets up the package json scripts', () => {
+    // eslint-disable-next-line import/no-unresolved
+    const userPackageJSON = require.requireActual('../../cli-test/package.json');
+    const scripts = userPackageJSON.scripts;
+    expect(scripts.dev).toBe('kyt dev');
+    expect(scripts.build).toBe('kyt build');
+    expect(scripts.test).toBe('kyt test');
+    expect(scripts.lint).toBe('kyt lint');
+    expect(scripts['lint-style']).toBe('kyt lint-style');
+    expect(scripts.proto).toBe('kyt proto');
+    expect(scripts['kyt:help']).toBe('kyt --help');
+  });
+
+  // afterAll(() => {
+  //   const shell = require.requireActual('shelljs');
+  //   shell.rm('-rf', 'cli-test');
+  // });
+});
