931af3b7da166da75e62af92b65adb9c2c3ab639
Jared McDonald <jared.r.mcdonald@gmail.com>
2016-10-08 13:00:54 -0400

    use modifyBabelConfig in jest preprocessor

--- utils/jest/preprocessor.js
+++ utils/jest/preprocessor.js
@@ -1,17 +1,7 @@
-const mergeAll = require('ramda').mergeAll;
 const babel = require('../../config/babel')();
 const babelJest = require('babel-jest');
-const buildConfigs = require('../buildConfigs');
 const config = require('../kytConfig')();
 
-const { clientConfig } = buildConfigs(config);
-
-// Merge userland babel config with our babel config
-// This should go away after https://github.com/NYTimes/kyt/issues/134
-const clientBabelConfig = clientConfig.module.loaders
-  .find(loader => loader.loader === 'babel-loader')
-  .query;
-
-const babelConfigForJest = mergeAll([{}, babel, clientBabelConfig]);
+const babelConfigForJest = config.modifyBabelConfig(babel);
 
 module.exports = babelJest.createTransformer(babelConfigForJest);
