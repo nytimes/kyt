ba5e9390629362a0257022cae91296985202add8
Price <carolyn.price@nytimes.com>
2016-10-11 16:54:14 -0400

    remove console logs

--- e2e_tests/tests/cli.test.js
+++ e2e_tests/tests/cli.test.js
@@ -22,9 +22,7 @@ describe('KYT CLI', () => {
   });
   it('sets up a starter-kyt', () => {
     let setupURL = 'git@github.com:NYTimes/kyt-starter-test.git';
-    console.log('TESTING', process.env.TEST_VALUE, process.env.TEST_TOKEN);
     if (process.env.TEST_TOKEN) {
-      console.log('it exists');
       setupURL = `https://${process.env.TEST_TOKEN}@github.com/NYTimes/kyt-starter-test.git`;
     }
     const output = shell.exec(`node_modules/.bin/kyt setup -r ${setupURL}`);
