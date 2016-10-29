f915b4b3ece1ab9b20021337dd76372710741a89
Zachary Lumpkins <zdoc01@gmail.com>
2016-10-29 15:47:02 -0700

    Support plugin/preset definitions without prefixes in .babelrc (#303)

--- config/babel.js
+++ config/babel.js
@@ -1,18 +1,39 @@
 const path = require('path');
 const fs = require('fs');
 
+/**
+ * Support preset and plugin definitions in .babelrc
+ * without the babel-* prefixes.
+ *
+ * E.g. {presets: ['babel-preset-react']} --> {presets: ['react']}
+ *
+ * @param  {String} prefix One of either 'babel-plugin' or 'babel-preset'
+ * @param  {String} dep    The dependency
+ * @return {String}        The normalized dependency
+ */
+const normalizeDep = (prefix, dep) => {
+  let resolved;
+  if (dep.indexOf('babel') !== 0) {
+    try {
+      resolved = require.resolve(`${prefix}-${dep}`);
+    } catch (e) { /* eslint-disable no-empty */ }
+  }
+
+  return resolved || require.resolve(dep);
+};
+
 // Uses require.resolve to add the full paths to all of the plugins
 // and presets, making sure that we handle the new array syntax.
 const resolvePluginsPresets = (babelGroup) => {
-  const resolve = (dep) => {
+  const resolve = (prefix, dep) => {
     if (typeof dep === 'object') {
-      dep[0] = require.resolve(dep[0]);
+      dep[0] = normalizeDep(prefix, dep[0]);
       return dep;
     }
-    return require.resolve(dep);
+    return normalizeDep(prefix, dep);
   };
-  babelGroup.plugins = (babelGroup.plugins || []).map(resolve);
-  babelGroup.presets = (babelGroup.presets || []).map(resolve);
+  babelGroup.plugins = (babelGroup.plugins || []).map(resolve.bind(null, 'babel-plugin'));
+  babelGroup.presets = (babelGroup.presets || []).map(resolve.bind(null, 'babel-preset'));
 };
 
 module.exports = (options) => {
