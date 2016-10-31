01589cea8c357a47d01c28976cb24e45f5fd794a
Adroop <gerasym198@gmail.com>
2016-10-31 21:49:09 +0200

    Update index.js

--- cli/index.js
+++ cli/index.js
@@ -2,11 +2,11 @@
 
 // This module has to be supported by old versions of node, so no ES6
 /* eslint-disable no-var, prefer-arrow-callback, prefer-template, no-console */
-var minNodeVersion = 6;
+var minNodeVersions = 6;
 
 // Make sure that the user has a compatible node version before loading the app.
-if (Number(process.versions.node.split('.')[0]) < minNodeVersion) {
-  console.error('kyt requires Node v' + minNodeVersion + '+');
+if (Number(process.versions.node.split('.')[0]) < minNodeVersions) {
+  console.error('kyt requires Node v' + minNodeVersions + '+');
   console.info('Need to run multiple versions of node? Check out nvm');
   process.exit(1);
 }
