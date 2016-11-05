7f04e50d5c21cf0c1c40fab9378cbb838352235c
Price <carolyn.price@nytimes.com>
2016-11-05 14:07:36 -0400

    even move DebugGing

--- cli/actions/setup.js
+++ cli/actions/setup.js
@@ -143,10 +143,12 @@ module.exports = (config, flags, args) => {
 
   // Cleans and reinstalls node modules.
   const installUserDependencies = () => {
-    logger.info('Cleaning node modules and reinstalling. This may take a couple of minutes...');
+    logger.info('Cleaning node modules and reinstalling. This may take a couple of mins...');
     const result = shell.exec(`rm -rf ${userNodeModulesPath} && ${ypm} install`);
     // eslint-disable-next-line
+    logger.info('testing');
     logger.info(`HELLO ${result}`);
+    logger.info(`hi ${result.code}`);
     if (result.code !== 0) {
       fs.writeFileSync(userPackageJSONPath, JSON.stringify(oldPackageJSON, null, 2));
       logger.error('An error occurred when trying to install node modules', result.stderr);
