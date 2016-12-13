f1b01ede9187940f11da6ead92970355618b1c80
Jared McDonald <jared.mcdonald@nytimes.com>
2016-12-13 13:31:33 -0500

    webpack should default to babel-preset-kyt-core if no user babelrc exists

--- packages/kyt-core/config/__tests__/webpack.test.js
+++ packages/kyt-core/config/__tests__/webpack.test.js
@@ -1,7 +1,14 @@
+const shell = {
+  test: jest.fn(),
+};
+
+jest.setMock('shelljs', shell);
+
 const devClientConfig = require('../webpack.dev.client');
 const devServerConfig = require('../webpack.dev.server');
 const prodClientConfig = require('../webpack.prod.client');
 const prodServerConfig = require('../webpack.prod.server');
+const baseConfig = require('../webpack.base');
 
 describe('webpack.dev.client', () => {
   it('has babel-polyfill as first entry in entry.main array', () => {
@@ -28,5 +35,21 @@ describe('webpack.prod.server', () => {
   it('has babel-polyfill as first entry in entry.main array', () => {
     const config = prodServerConfig({ publicPath: '/' });
     expect(config.entry.main[0]).toBe('babel-polyfill');
+  });
+});
+
+describe('webpack.base', () => {
+  it('doesn\'t set up a babel preset if a .babelrc exists', () => {
+    shell.test.mockImplementationOnce(() => true);
+    const config = baseConfig({ clientURL: {}, publicPath: '/' });
+    const babelLoader = config.module.rules.find(({ loader }) => loader === 'babel-loader');
+    expect(babelLoader.options.presets).toBeUndefined();
+  });
+  it('sets up kyt-core babel preset if a .babelrc exists', () => {
+    shell.test.mockImplementationOnce(() => false);
+    const config = baseConfig({ clientURL: {}, publicPath: '/' });
+    const babelLoader = config.module.rules.find(({ loader }) => loader === 'babel-loader');
+    expect(babelLoader.options.presets.length).toBe(1);
+    expect(babelLoader.options.presets[0]).toMatch(/babel-preset-kyt-core/);
   });
 });
