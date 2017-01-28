beb21a81474c4c012c470df69aaff99ef20b4eb2
Carrie Price <ccpricenytimes@users.noreply.github.com>
2017-01-28 17:33:47 -0500

    Adding identity object property; fixing preprocessor bug (#367)

--- packages/kyt-core/config/jest.js
+++ packages/kyt-core/config/jest.js
@@ -9,8 +9,8 @@ module.exports = (rootDir, aliases = {}) => ({
     {
       '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
           resolveFromUtils('file.stub'),
-      '\\.(css|scss)$':
-          resolveFromUtils('style.stub'),
+      '^[./a-zA-Z0-9!&$_-]+\\.(css|scss)$':
+          'identity-obj-proxy',
       // when this is removed from the base webpack config, we can likely
       // remove the runtime and include the polyfill in the test environment
       'babel-runtime': require.resolve('babel-plugin-transform-runtime'),
