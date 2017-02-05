3263cd6c37f2e271b0e6e02446995da0eb25d242
Price <carolyn.price@nytimes.com>
2017-02-04 19:33:48 -0500

    First pass at interactive setup, e2e tests broken

--- packages/kyt-cli/cli/commands.js
+++ packages/kyt-cli/cli/commands.js
@@ -12,11 +12,8 @@ const loadArgsAndDo = (action) => {
 program
   .command('setup')
   .description('Generate a project from a github url to get started.')
-  .option('-d, --directory <path>', 'Optional: Creates the given directory name and installs there. Defaults to your current working directory.')
-  .option('-r, --repository [address]', 'Optional: Github repository address')
-  .option('-k, --kyt-version [version]', 'Optional: Version of kyt-core to install')
-  .option('-p, --package-manager <npm|yarn>', 'Optional: Specify which package manager to use (npm or yarn). Defaults to yarn if it is installed globally.')
-  .option('--repository-path [path]', 'Optional: path within a repository for the main starter kyt directory. Default is the root directory of a git clone.')
+  .option('-k, --kyt-version [version]', 'For Local Dev: Version of kyt-core to install')
+  .option('--repository-path [path]', 'For Local Dev: path within a repository for the main starter kyt directory. Default is the root directory of a git clone.')
   .action(() => loadArgsAndDo(setupAction));
 
 program
