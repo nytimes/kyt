c2c1e87cd5f57a2863295db27b9e9044a9ed5b04
Julian V. Modesto <julianvmodesto@gmail.com>
2017-02-14 00:34:33 -0500

    Upgrade eslint

--- packages/kyt-starter-universal/src/routes/index.js
+++ packages/kyt-starter-universal/src/routes/index.js
@@ -9,13 +9,13 @@ import App from '../components/App';
 // https://webpack.js.org/guides/migrating/#code-splitting-with-es2015
 
 const importHome = (nextState, cb) => {
-  import('../components/Home') // eslint-disable-line import/no-named-as-default
+  import('../components/Home')
     .then(module => cb(null, module.default))
     .catch((e) => { throw e; });
 };
 
 const importTools = (nextState, cb) => {
-  import('../components/Tools') // eslint-disable-line import/no-named-as-default
+  import('../components/Tools') // eslint-disable-line
     .then(module => cb(null, module.default))
     .catch((e) => { throw e; });
 };
