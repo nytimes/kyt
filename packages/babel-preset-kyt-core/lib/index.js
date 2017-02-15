a81a8c4344bddd00d05b89c32dfe0b2c859e7cd9
Julian V. Modesto <julianvmodesto@gmail.com>
2017-02-15 00:48:20 -0500

    Try babel-eslint + syntax-dynamic-import in babel-preset-kyt-core

--- packages/babel-preset-kyt-core/lib/index.js
+++ packages/babel-preset-kyt-core/lib/index.js
@@ -1,15 +1,24 @@
 var babelPresetLatest = require('babel-preset-latest');
 var babelTransformRuntime = require('babel-plugin-transform-runtime');
 var babelTransformModules = require('babel-plugin-transform-es2015-modules-commonjs');
+var babelSyntaxDynamicImport = require('babel-plugin-syntax-dynamic-import');
+var babelEslint = require('babel-eslint');
 
 module.exports = function getPresetCore(context, opts) {
   opts = opts || {};
   return {
+    parser: babelEslint,
+    parserOptions: {
+      sourceType: 'module',
+      allowImportExportEverywhere: true,
+    },
+
     // modules are handled by webpack, don't transform them
     presets: [[babelPresetLatest, { modules: false }]],
 
     // provide the ability to opt into babel-plugin-transform-runtime inclusion
-    plugins: [opts.includeRuntime === true && babelTransformRuntime].filter(Boolean),
+    plugins: [opts.includeRuntime === true && babelTransformRuntime].filter(Boolean)
+      .push(babelSyntaxDynamicImport),
 
     env: {
       test: {
