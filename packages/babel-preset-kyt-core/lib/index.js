e4fc3deba72c96914c5290a397619efc7a0e7749
Julian V. Modesto <julianvmodesto@gmail.com>
2017-02-16 22:43:50 -0500

    Put babel-eslint in the right place (maybe)

--- packages/babel-preset-kyt-core/lib/index.js
+++ packages/babel-preset-kyt-core/lib/index.js
@@ -2,17 +2,10 @@ var babelPresetLatest = require('babel-preset-latest');
 var babelTransformRuntime = require('babel-plugin-transform-runtime');
 var babelTransformModules = require('babel-plugin-transform-es2015-modules-commonjs');
 var babelSyntaxDynamicImport = require('babel-plugin-syntax-dynamic-import');
-var babelEslint = require('babel-eslint');
 
 module.exports = function getPresetCore(context, opts) {
   opts = opts || {};
   return {
-    parser: babelEslint,
-    parserOptions: {
-      sourceType: 'module',
-      allowImportExportEverywhere: true,
-    },
-
     // modules are handled by webpack, don't transform them
     presets: [[babelPresetLatest, { modules: false }]],
 
