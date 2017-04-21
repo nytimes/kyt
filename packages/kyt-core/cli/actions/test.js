cf49a5a3518ed02ed3d488796fe68555c069250c
Joseph Kohlmann <kohlmannj@gmail.com>
2017-04-21 14:17:18 -0400

    Update `test` command with custom `—collectCoverageFromPattern` flag
    This custom flag takes the test path / pattern intended to be passed to jest-cli and also passes it to the `--collectCoverageFrom` option. It’s an effective way to run a subset of your tests _and_ see coverage for that same subset.
    
    Usage example:
    
    `kyt test -- shared/ButtonModal/**/*.js --collectCoverageFromPattern --watch`
    
    `kyt-core/cli/actions/test` now translates those flags into this equivalent command:
    `kyt test -- shared/ButtonModal/**/*.js --coverage --collectCoverageFrom=shared/ButtonModal/**/*.js --watch`

--- packages/kyt-core/cli/actions/test.js
+++ packages/kyt-core/cli/actions/test.js
@@ -8,6 +8,31 @@ const jestConfigBuilder = require('../../config/jest');
 const { srcPath } = require('kyt-utils/paths')();
 const buildConfigs = require('../../utils/buildConfigs');
 
+const handleCollectCoverageFromPatternFlag = (flags) => {
+  // Check for custom `--collectCoverageFromPattern` flag (with a pattern or test path arg, too)
+  const collectCoverageFromPatternFlagIndex =
+    flags.findIndex(flag => flag.indexOf('--collectCoverageFromPattern') >= 0)
+  ;
+
+  const pathOrPatternIndex =
+    flags.findIndex(flag => flag.indexOf('-') !== 0 && flag.indexOf('--') !== 0)
+  ;
+
+  if (collectCoverageFromPatternFlagIndex >= 0 && pathOrPatternIndex >= 0) {
+    // Remove proprietary flag `--collectCoverageFromPattern` and add:
+    // - The `--coverage` flag
+    // - The `--collectCoverageFrom` flag along with the path or pattern arg
+    flags = [
+      ...flags.filter((flag, index) => (
+        index !== collectCoverageFromPatternFlagIndex
+      )),
+      '--coverage',
+      `--collectCoverageFrom=${flags[pathOrPatternIndex]}`
+    ];
+  }
+
+  return flags;
+};
 
 module.exports = (config, flags) => {
   // Comment the following to see verbose shell ouput.
@@ -22,6 +47,9 @@ module.exports = (config, flags) => {
   // Build Jest config
   let jestConfig = jestConfigBuilder(srcPath, aliases);
   jestConfig = config.modifyJestConfig(clone(jestConfig));
+
+  // Check for custom `--collectCoverageFromPattern` flag (and possibly modify `flags`)
+  flags = handleCollectCoverageFromPatternFlag(flags);
 
   // Run Jest
   jest.run(['--config', JSON.stringify(jestConfig), ...flags]);
