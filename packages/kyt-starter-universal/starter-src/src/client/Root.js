20cb4ff6ddf8ee1789071f01195c25c4955a8772
Yuzhou Huang <happyjoe2008@gmail.com>
2017-04-28 22:12:44 -0500

    Updated RR4

--- packages/kyt-starter-universal/starter-src/src/client/Root.js
+++ packages/kyt-starter-universal/starter-src/src/client/Root.js
@@ -1,13 +1,12 @@
 
 import React from 'react';
-import Router from 'react-router/lib/Router';
-import browserHistory from 'react-router/lib/browserHistory';
+import Router from 'react-router/lib/BrowserRouter';
 import routes from '../routes';
 
 // We need a Root component for React Hot Loading.
 function Root() {
   return (
-    <Router history={browserHistory} routes={routes} />
+    <Router routes={routes} />
   );
 }
 
