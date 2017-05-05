0761f28db66657a87a741dd015f2866a5fb10f7f
Joe Huang <happyjoe2008@gmail.com>
2017-05-05 03:16:30 -0700

    testing git issues

--- packages/kyt-starter-universal/starter-src/src/client/Root.js
+++ packages/kyt-starter-universal/starter-src/src/client/Root.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router';
 import routes from '../routes';
 
+//testing
+
 // We need a Root component for React Hot Loading.
 function Root() {
   return (
