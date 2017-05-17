dbc94b742678453e426566b1438db80b94ee2c3b
TheLordHighExecutioner <happyjoe2008@gmail.com>
2017-05-16 21:22:19 -0700

    Fix hmr

--- packages/kyt-starter-universal/starter-src/src/routes/index.js
+++ packages/kyt-starter-universal/starter-src/src/routes/index.js
@@ -3,6 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 import Home from '../components/Home';
 import Tools from '../components/Tools';
 
+// Unfortunately, HMR breaks when we dynamically resolve		 +export default function App() {
+// routes so we need to require them here as a workaround.
+// https://github.com/gaearon/react-hot-loader/issues/288
+if (module.hot) {
+    require('../Home');    // eslint-disable-line global-require
+    require('../Tools');   // eslint-disable-line global-require
+}
+
 export default (
   <Switch>
     <Route exact path="/" component={Home} />
