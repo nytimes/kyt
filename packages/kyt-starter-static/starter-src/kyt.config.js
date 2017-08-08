ba42e67fa2c431d9a214bdb6eaba83d0b90b52d5
delambo <delambo@gmail.com>
2017-08-08 14:22:09 -0400

    fixes (patches 0.5.1) static starter kyt script ordering bug

--- packages/kyt-starter-static/starter-src/kyt.config.js
+++ packages/kyt-starter-static/starter-src/kyt.config.js
@@ -9,6 +9,13 @@ module.exports = {
       config.plugins.push(
         new HtmlWebpackPlugin({
           template: 'src/index.ejs',
+          // Sort the chunks so that the scripts are added in the correct order.
+          chunksSortMode: (chunk1, chunk2) => {
+            const orders = ['manifest', 'vendor', 'main'];
+            const order1 = orders.indexOf(chunk1.names[0]);
+            const order2 = orders.indexOf(chunk2.names[0]);
+            return order1 - order2;
+          },
         })
       );
     }
