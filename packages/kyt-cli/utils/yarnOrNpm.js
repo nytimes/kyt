f6808600499f192cc7566c0e7d72b15ae2cc336a
Sam Kvale <skvale@widen.com>
2018-05-08 20:58:44 -0500

    removes yarn version command output

--- packages/kyt-cli/utils/yarnOrNpm.js
+++ packages/kyt-cli/utils/yarnOrNpm.js
@@ -1,6 +1,6 @@
 // Checks to see if yarn is installed or not
 const shell = require('shelljs');
 
-const isYarnInstalled = () => shell.exec('yarn --version').code === 0;
+const isYarnInstalled = () => shell.exec('[ -x "$(command -v yarn)" ]').code === 0;
 
 module.exports = () => (isYarnInstalled() ? 'yarn' : 'npm');
