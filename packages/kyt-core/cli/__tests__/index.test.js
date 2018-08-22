086c80b8dbd1b70e911fbaf964ca3079fbf41097
Matthew DeLambo <delambo@gmail.com>
2018-08-22 11:15:14 -0400

    Update the Jest dependencies (#568)
    * update jest dependencies

--- packages/kyt-core/cli/__tests__/index.test.js
+++ packages/kyt-core/cli/__tests__/index.test.js
@@ -3,11 +3,10 @@
 jest.mock('../commands', () => {});
 
 it('bails when min node version is not met', () => {
-  global.process.exit = jest.fn();
   global.console.error = jest.fn();
   global.console.info = jest.fn();
-  delete global.process.versions.node;
-  global.process.versions.node = '4.0';
+  global.process = { versions: { node: '4.0' } };
+  global.process.exit = jest.fn();
   require('../index');
 
   expect(global.console.error).toBeCalled();
