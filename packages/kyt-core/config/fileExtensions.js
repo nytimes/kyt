5c7ca1a63e63d9c480cfdcefb4d9a320e65cb11f
Joseph Kohlmann <kohlmannj@gmail.com>
2018-08-29 16:53:22 -0400

    fileExtensions.js: add ^[./a-zA-Z0-9!&$_-]+ (#570)
    This change to the fileExtensions regex works around an issue seen in wf-project-vi: https://github.com/nytm/wf-project-vi/pull/5957#issuecomment-417087289

--- packages/kyt-core/config/fileExtensions.js
+++ packages/kyt-core/config/fileExtensions.js
@@ -1,2 +1,2 @@
 module.exports =
-  '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ico)$';
+  '^[./a-zA-Z0-9!&$_-]+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ico)$';
