731b5812e50c721b177482068df9bf529b2652d9
Scott Taylor <scott.c.taylor@mac.com>
2019-01-31 13:38:50 -0500

    Upgrade to Jest 24, ditch babel-core (#586)

--- packages/kyt-core/cli/actions/test.js
+++ packages/kyt-core/cli/actions/test.js
@@ -1,7 +1,7 @@
 // Command to run tests with Jest
 
+const child = require('child_process');
 const clone = require('lodash.clonedeep');
-const jest = require('jest');
 const shell = require('shelljs');
 const { srcPath } = require('kyt-utils/paths')();
 const jestConfigBuilder = require('../../config/jest');
@@ -24,5 +24,7 @@ module.exports = (config, flags) => {
   process.env.KYT_ENV_TYPE = 'test';
 
   // Run Jest
-  jest.run(['--config', JSON.stringify(jestConfig), ...flags]);
+  child.spawn('jest', ['--config', JSON.stringify(jestConfig), ...flags], {
+    stdio: 'inherit',
+  });
 };
