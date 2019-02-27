1614504864069f4339439f4e38150d9183266165
Scott Taylor <scott.c.taylor@mac.com>
2019-02-27 14:30:31 -0500

    clean up Loadable Webpack plugin and add entrypoints to artifact

--- packages/kyt-starter-universal/starter-src/src/server/template.js
+++ packages/kyt-starter-universal/starter-src/src/server/template.js
@@ -1,35 +1,28 @@
 /* eslint-disable prefer-template, max-len */
 
-const getDeferScript = src => (src ? `<script defer src="${src}"></script>` : '');
+const getDeferScript = src => `<script defer src="${src}"></script>`;
 
-export default ({
-  html,
-  mainCSSBundle,
-  runtimeJSBundle,
-  vendorJSBundle,
-  mainJSBundle,
-  bundles,
-}) => `
+export default ({ html, bundles }) => `
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
     <meta charSet='utf-8' />
     <meta httpEquiv="Content-Language" content="en" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link id="favicon" rel="shortcut icon" href="/kyt-favicon.png" sizes="16x16 32x32" type="image/png" />
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css">
-    ${mainCSSBundle ? `<link rel="stylesheet" type="text/css" href="${mainCSSBundle}">` : ''}
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css" />
+    ${bundles.cssBundle ? `<link rel="stylesheet" href="${bundles.cssBundle}" />` : ''}
     ${bundles.styles.map(e => `<link rel="stylesheet" href="${e}">`).join('\n')}
     <title>Universal React Starter Kyt</title>
   </head>
   <body>
     <div id="root">${html}</div>
-    ${getDeferScript(runtimeJSBundle)}
-    ${getDeferScript(vendorJSBundle)}
+    ${bundles.runtimeBundle ? getDeferScript(bundles.runtimeBundle) : ''}
+    ${bundles.vendorBundle ? getDeferScript(bundles.vendorBundle) : ''}
     ${bundles.scripts.map(getDeferScript).join('\n')}
-    ${getDeferScript(mainJSBundle)}
+    ${bundles.entryBundle ? getDeferScript(bundles.entryBundle) : ''}
   </body>
 </html>
 `;
