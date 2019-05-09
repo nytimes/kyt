eefa5c563cd24f267980a1c4ed4b59b66397f3f0
Scott Taylor <scott.c.taylor@mac.com>
2019-05-09 18:07:31 -0400

    fix tests

--- packages/kyt-core/utils/__tests__/webpackCompiler.test.js
+++ packages/kyt-core/utils/__tests__/webpackCompiler.test.js
@@ -1,9 +1,16 @@
 /* eslint-disable global-require */
 
-const plugin = jest.fn();
+const hooks = {
+  beforeRun: {
+    tap: jest.fn(),
+  },
+  done: {
+    tap: jest.fn(),
+  },
+};
 const webpackMock = jest.fn(obj => {
   if (obj.error) throw new Error('test error');
-  return { plugin };
+  return { hooks };
 });
 jest.setMock('webpack', webpackMock);
 const cb = jest.fn();
@@ -19,29 +26,29 @@ describe('webpackCompiler', () => {
 
   describe('when called with valid webpack config', () => {
     const compiler = webpackCompiler({ test: 'config' }, cb);
-    const pluginBeforeRun = plugin.mock.calls[0][1];
-    const pluginDone = plugin.mock.calls[1][1];
+    const beforeRunHook = hooks.beforeRun.tap.mock.calls[0][1];
+    const doneHook = hooks.done.tap.mock.calls[0][1];
 
     it('calls webpack and defines a done and before-run function', () => {
       expect(logger.task).toBeCalledWith('Server webpack configuration compiled');
-      expect(typeof pluginDone).toBe('function');
-      expect(typeof pluginBeforeRun).toBe('function');
+      expect(typeof doneHook).toBe('function');
+      expect(typeof beforeRunHook).toBe('function');
     });
 
     it('sets the process KYT_ENV_TYPE before running', () => {
-      pluginBeforeRun({}, () => {});
+      beforeRunHook({}, () => {});
       expect(process.env.KYT_ENV_TYPE).toEqual('server');
     });
 
     it('then displays any errors', () => {
-      pluginDone({ hasErrors: () => true, hasWarnings: () => true });
+      doneHook({ hasErrors: () => true, hasWarnings: () => true });
 
       expect(logger.error).toBeCalled();
       expect(logger.info).toBeCalledWith('See webpack error above');
     });
 
     it('then displays any warnings', () => {
-      pluginDone({
+      doneHook({
         hasErrors: () => false,
         hasWarnings: () => true,
         toJson: () => ({ warnings: [] }),
@@ -50,12 +57,12 @@ describe('webpackCompiler', () => {
     });
 
     it('removes the process KYT_ENV_TYPE when done', () => {
-      pluginDone({ hasErrors: () => true, hasWarnings: () => true });
+      doneHook({ hasErrors: () => true, hasWarnings: () => true });
       expect(process.env.KYT_ENV_TYPE).toEqual(undefined);
     });
 
     it('calls the provided callback', () => {
-      pluginDone({ hasErrors: () => false, hasWarnings: () => false });
+      doneHook({ hasErrors: () => false, hasWarnings: () => false });
       expect(logger.task).toBeCalledWith('Server build successful');
       expect(cb).toBeCalled();
     });
@@ -66,12 +73,12 @@ describe('webpackCompiler', () => {
   });
 
   describe('when called with a target=web, webpack config', () => {
-    plugin.mockReset();
+    hooks.beforeRun.tap.mockReset();
     webpackCompiler({ test: 'config', target: 'web' }, cb);
-    const pluginBeforeRun = plugin.mock.calls[0][1];
+    const beforeRunHook = hooks.beforeRun.tap.mock.calls[0][1];
 
     it('sets the process KYT_ENV_TYPE to client before running', () => {
-      pluginBeforeRun({}, () => {});
+      beforeRunHook({}, () => {});
       expect(process.env.KYT_ENV_TYPE).toEqual('client');
     });
   });
