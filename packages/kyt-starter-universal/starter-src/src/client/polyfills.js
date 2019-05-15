67c11f95270dee7b2b0be11dff44264559fc987c
delambo <delambo@gmail.com>
2019-05-15 10:56:09 -0400

    ignore stable import

--- packages/kyt-starter-universal/starter-src/src/client/polyfills.js
+++ packages/kyt-starter-universal/starter-src/src/client/polyfills.js
@@ -1,5 +1,6 @@
 // kyt dependency for polyfills, using babel-preset-env.
 
 // Polyfill for node that works with `babel-preset-env` `useBuiltIns` option.
+// eslint-disable-next-line import/no-unresolved
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
--- packages/kyt-starter-universal/starter-src/src/server/polyfills.js
+++ packages/kyt-starter-universal/starter-src/src/server/polyfills.js
@@ -1,5 +1,6 @@
 // kyt dependency for polyfills, using babel-preset-env.
 
 // General polyfill for es6+ features that works with `babel-preset-env` `useBuiltIns` option.
+// eslint-disable-next-line import/no-unresolved
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
