04a095cf4bb5b7da2be9e485d23ae8976493de0b
Scott Taylor <scott.c.taylor@mac.com>
2019-11-05 13:26:39 -0500

    create Jest preset for projects with styled components

---
+++ packages/jest-preset-kyt-styled/.stylelintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  processors: ['stylelint-processor-styled-components'],
+  extends: ['stylelint-config-standard', 'stylelint-config-styled-components'],
+  rules: {
+    'declaration-colon-newline-after': null,
+    'function-name-case': null,
+    'no-descending-specificity': null,
+    'selector-list-comma-newline-after': null,
+    'value-list-comma-newline-after': null,
+  },
+};
---
+++ packages/jest-preset-kyt-styled/jest-preset.js
@@ -0,0 +1,6 @@
+module.exports = {
+  runner: 'jest-runner-stylelint',
+  displayName: 'stylelint',
+  moduleFileExtensions: ['js'],
+  testMatch: ['<rootDir>/src/**/**/styled.js'],
+};
