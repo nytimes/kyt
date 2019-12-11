ec877844cd6446a3826961c410b34e7bad9465f3
Juan M. García González <juan.garcia@nytimes.com>
2019-12-10 23:18:37 -0400

    STRIK-1008: fully remove ES6 from client.js (#616)

--- packages/kyt-runtime/client.js
+++ packages/kyt-runtime/client.js
@@ -1,3 +1,4 @@
-var { preloadReady } = require('./lib/loadable');
+// this file doesn't get transpiled. Please avoid ES6 syntax.
+var preloadReady = require('./lib/loadable').preloadReady; // eslint-disable-line
 
 exports.preloadDynamicImports = preloadReady;
