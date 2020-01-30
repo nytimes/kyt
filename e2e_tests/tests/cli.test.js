72f44ff848a47cf0ee69303459b63415287fdbd0
Scott Taylor <scott.c.taylor@mac.com>
2020-01-30 17:52:18 -0500

    Update cli.test.js

--- e2e_tests/tests/cli.test.js
+++ e2e_tests/tests/cli.test.js
@@ -31,9 +31,6 @@ describe('KYT CLI', () => {
       expect(scripts.start).toBe('node build/server/main.js');
       expect(scripts.build).toBe('kyt build');
       expect(scripts.test).toBe('kyt test');
-      expect(scripts.lint).toBe('npm run lint-script && npm run lint-style');
-      expect(scripts['lint-style']).toBe('kyt lint-style');
-      expect(scripts['lint-script']).toBe('kyt lint-script');
       expect(scripts.proto).toBe('kyt proto');
       expect(scripts['kyt:help']).toBe('kyt --help');
     });
