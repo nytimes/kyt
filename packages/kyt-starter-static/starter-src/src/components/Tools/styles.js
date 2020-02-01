7ec0855a8a6a3b27045ed7c0691464b027de75fb
Scott Taylor <scott.c.taylor@mac.com>
2020-02-01 17:49:11 -0500

    cruft

--- packages/kyt-starter-static/starter-src/src/components/Tools/styles.js
+++
@@ -1,8 +0,0 @@
-import { css } from 'pretty-lights';
-
-// eslint-disable-next-line
-export const toolClass = css`
-  & + & {
-    padding-top: 5px;
-  }
-`;
--- packages/kyt-starter-universal/starter-src/src/components/Home/styles.js
+++
@@ -1,9 +0,0 @@
-import { css } from 'pretty-lights';
-import { padding } from '../../shared-styles/variables';
-
-// eslint-disable-next-line
-export const paragraphClass = css`
-  & + & {
-    padding-top: ${padding}px;
-  }
-`;
