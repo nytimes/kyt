113feb62357e61417e9ab602f1de6e629d98279b
Scott Taylor <scott.c.taylor@mac.com>
2020-02-01 02:47:49 -0500

    move kyt-starter-static to CSS-in-JS

---
+++ packages/kyt-starter-static/starter-src/test/jest.test.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  moduleNameMapper: {
+    '^[./a-zA-Z0-9!&$_-]+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ico)$': require.resolve(
+      './jest.stub.js'
+    ),
+  },
+  setupFiles: ['raf/polyfill'],
+  setupFilesAfterEnv: [require.resolve('./jest.setup.js')],
+  snapshotSerializers: ['enzyme-to-json/serializer', 'pretty-lights/jest'],
+  rootDir: path.resolve(__dirname, '..'),
+};
