f115f4c7e11693be1e7a25bfa2f6e797d9301f02
Scott Taylor <scott.c.taylor@mac.com>
2020-02-01 05:17:04 -0500

    drop unused e2e fixtures (#630)

--- e2e_tests/fixtures/lintScript-default/src/test.js
+++
@@ -1,3 +0,0 @@
-const test = 1;
-
-module.exports = test;
--- e2e_tests/fixtures/test-modify-config/kyt.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  modifyJestConfig: config => {
-    delete config.testRegex;
-    config.testRegex = '(.*(blargh|spec))\\.js$';
-    return config;
-  },
-};
