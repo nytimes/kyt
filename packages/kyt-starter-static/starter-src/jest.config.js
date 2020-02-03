fa552deae05471c59d9e3b2da82102e5b3949123
Scott Taylor <scott.c.taylor@mac.com>
2020-02-03 14:56:36 -0500

    cruft / upgrades

--- packages/kyt-starter-static/starter-src/jest.config.js
+++ packages/kyt-starter-static/starter-src/jest.config.js
@@ -1,14 +1,6 @@
-const path = require('path');
-
 module.exports = {
   projects: [
     require.resolve('./test/jest.test.config'),
     require.resolve('./test/jest.style.config'),
   ],
-  cacheDirectory: path.resolve(__dirname, '.caches/jest'),
-  haste: {
-    computeSha1: true,
-    providesModuleNodeModules: [],
-    throwOnModuleCollision: false,
-  },
 };
