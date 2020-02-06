9f765e207e9f40c18d7334ae439e1d2ca87bf4cb
Scott Taylor <scott.c.taylor@mac.com>
2020-02-06 17:55:19 -0500

    tweaks

--- packages/kyt-starter-server/starter-src/src/components/Home/index.js
+++ packages/kyt-starter-server/starter-src/src/components/Home/index.js
@@ -5,8 +5,8 @@ function Home() {
   return (
     <section>
       <p className={styles.paragraphClass}>
-        Welcome to the <strong>Universal React Starter-kyt</strong>. This starter kyt should serve
-        as the base for an advanced, server-rendered React app.
+        Welcome to the <strong>React Server Starter-kyt</strong>. This starter kyt should serve as
+        the base for an server-rendered React app.
       </p>
       <p className={styles.paragraphClass}>
         Check out the Tools section for an outline of the libraries that are used in this
