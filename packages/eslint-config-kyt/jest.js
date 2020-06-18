c56fc48cf73e919ca61fcbe3b35183b4245d1157
Scott Taylor <scott.c.taylor@mac.com>
2020-06-18 14:16:09 -0400

    break eslint config into selective subsets

---
+++ packages/eslint-config-kyt/jest.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'plugin:jest-formatting/strict',
+    'plugin:json/recommended',
+  ],
+
+  env: {
+    jest: true,
+  },
+};
---
+++ packages/eslint-config-kyt/react.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: ['prettier/react'],
+
+  plugins: ['react-hooks'],
+
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+
+  rules: {
+    'react/sort-comp': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-fragments': 'error',
+    'react/jsx-no-useless-fragment': 'error',
+    'react/require-extension': 'off',
+    'react/destructuring-assignment': 'off',
+    'react/no-access-state-in-setstate': 'off',
+    'react-hooks/exhaustive-deps': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'jsx-a11y/anchor-is-valid': [
+      'off',
+      {
+        components: ['Link'],
+      },
+    ],
+  },
+};
