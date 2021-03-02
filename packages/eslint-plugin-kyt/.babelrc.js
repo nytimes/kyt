b7eb5b0d15c883f8387a56e101320151f0c87116
Scott Taylor <scott.c.taylor@mac.com>
2021-03-02 10:47:27 -0700

    chore: bump babel deps (again)

--- packages/eslint-plugin-kyt/.babelrc.js
+++ packages/eslint-plugin-kyt/.babelrc.js
@@ -1,4 +1,10 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: ['@babel/plugin-proposal-class-properties'],
+  presets: [
+    [
+      'babel-preset-kyt-react',
+      {
+        includeRuntime: true,
+      },
+    ],
+  ],
 };
