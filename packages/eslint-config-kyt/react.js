bb6684d8d6be40ab22ddce9802e3f486c0df588e
Scott Taylor <scott.c.taylor@mac.com>
2021-03-11 17:41:15 -0500

    chore: bump deps

--- packages/eslint-config-kyt/react.js
+++ packages/eslint-config-kyt/react.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['airbnb/rules/react', 'airbnb/rules/react-a11y', 'airbnb/hooks', 'prettier/react'],
+  extends: ['airbnb/rules/react', 'airbnb/rules/react-a11y', 'airbnb/hooks'],
 
   settings: {
     react: {
@@ -9,18 +9,35 @@ module.exports = {
   },
 
   rules: {
-    'react/sort-comp': 0,
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/jsx-fragments': 'error',
-    'react/jsx-no-useless-fragment': 'error',
-    'react/require-extension': 'off',
-    'react/destructuring-assignment': 'off',
-    'react/no-access-state-in-setstate': 'off',
     'jsx-a11y/anchor-is-valid': [
       'off',
       {
         components: ['Link'],
       },
     ],
+    // prettier/react < v8.0
+    'react/jsx-child-element-spacing': 'off',
+    'react/jsx-closing-bracket-location': 'off',
+    'react/jsx-closing-tag-location': 'off',
+    'react/jsx-curly-newline': 'off',
+    'react/jsx-curly-spacing': 'off',
+    'react/jsx-equals-spacing': 'off',
+    'react/jsx-first-prop-new-line': 'off',
+    'react/jsx-indent': 'off',
+    'react/jsx-indent-props': 'off',
+    'react/jsx-max-props-per-line': 'off',
+    'react/jsx-newline': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-props-no-multi-spaces': 'off',
+    'react/jsx-tag-spacing': 'off',
+    'react/jsx-wrap-multilines': 'off',
+    // our rules
+    'react/destructuring-assignment': 'off',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    'react/jsx-fragments': 'error',
+    'react/jsx-no-useless-fragment': 'error',
+    'react/no-access-state-in-setstate': 'off',
+    'react/require-extension': 'off',
+    'react/sort-comp': 'off',
   },
 };
