e167158603888f39614fa41426d5f8f1775336ce
delambo <delambo@gmail.com>
2022-11-01 10:10:41 -0400

    avoid stylelint + prettier rule clashes

--- packages/jest-preset-kyt-styled/src/.stylelintrc.js
+++ packages/jest-preset-kyt-styled/src/.stylelintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   processors: ['stylelint-processor-styled-components'],
-  extends: ['stylelint-config-standard', 'stylelint-config-styled-components'],
+  extends: ['stylelint-config-standard', 'stylelint-config-styled-components', 'stylelint-config-prettier'],
   rules: {
     'declaration-colon-newline-after': null,
     'declaration-no-important': true,
